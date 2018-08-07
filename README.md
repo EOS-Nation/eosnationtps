7dcd546249bb6e9bb299b0d36537fcd1d86ae3fb
Denis Carriere <carriere.denis@gmail.com>
2018-08-07 13:59:23 -0400

    Add default config

--- README.md
+++ README.md
@@ -26,7 +26,7 @@ $ npm install
 
 Add/edit the `.env` file.
 
-```env
+```conf
 # EOSIO Network
 EOSIO_HTTP_ENDPOINT = "https://api.eosn.io"
 EOSIO_CHAIN_ID = "aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906"
--- config.ts
+++ config.ts
@@ -1,6 +1,9 @@
+import * as fs from "fs";
+import * as path from "path";
 import dotenv from 'dotenv';
 
 dotenv.config();
+const envFilepath = path.join(__dirname, ".env")
 
 // EOSIO network
 export const httpEndpoint = process.env.EOSIO_HTTP_ENDPOINT || 'https://api.eosn.io'
@@ -20,16 +23,45 @@ export const permission = process.env.EOSIO_PERMISSION;
 export const keyProvider = process.env.EOSIO_KEY_PROVIDER;
 
 // Validate Credentials
-if (!actor || actor === "<ACCOUNT NAME>") { throw new Error("[EOSIO_ACTOR] is required"); }
-if (!permission) { throw new Error("[EOSIO_PERMISSION] is required"); }
-if (!keyProvider || keyProvider === "<PRIVATE KEY>") { throw new Error("[EOSIO_KEY_PROVIDER] is required"); }
+if (fs.existsSync(envFilepath)) {
+    if (!actor || actor === "<ACCOUNT NAME>") { throw new Error("[EOSIO_ACTOR] is required"); }
+    if (!permission) { throw new Error("[EOSIO_PERMISSION] is required"); }
+    if (!keyProvider || keyProvider === "<PRIVATE KEY>") { throw new Error("[EOSIO_KEY_PROVIDER] is required"); }
+}
 
 // Debug
-if (JSON.parse(process.env.EOSNATIONTPS_DEBUG || "true")) {
+if (JSON.parse(process.env.EOSNATIONTPS_DEBUG || "false")) {
     EOSNATIONTPS_MSG = 'EOS Nation TPS Debug'
     EOSNATIONTPS_INTERVAL_SECONDS = 3
     EOSNATIONTPS_ACTIONS = 1
     EOSNATIONTPS_QUEUE = 1
     EOSNATIONTPS_TRANSACTIONS = 1
     EOSNATIONTPS_START_TIME = Date.now()
+}
+
+if (!fs.existsSync(envFilepath)) {
+    fs.writeFileSync(envFilepath, `# EOSIO Network
+EOSIO_HTTP_ENDPOINT = "${httpEndpoint}"
+EOSIO_CHAIN_ID = "${chainId}"
+
+# EOS Nation TPS settings
+EOSNATIONTPS_MSG = "${EOSNATIONTPS_MSG}"
+EOSNATIONTPS_ACTIONS = ${EOSNATIONTPS_ACTIONS}
+EOSNATIONTPS_INTERVAL_SECONDS = ${EOSNATIONTPS_INTERVAL_SECONDS}
+EOSNATIONTPS_QUEUE = ${EOSNATIONTPS_QUEUE}
+EOSNATIONTPS_TRANSACTIONS = ${EOSNATIONTPS_TRANSACTIONS}
+
+# UTC 8/8/8 = 1533711600000
+EOSNATIONTPS_START_TIME = ${EOSNATIONTPS_START_TIME}
+
+# DEBUG mode
+# When true: set all values to 1 & start time to now
+# When false: values are not changed
+EOSNATIONTPS_DEBUG = false
+
+# User Credentials (Private)
+EOSIO_ACTOR = "<ACCOUNT NAME>"
+EOSIO_PERMISSION = "active"
+EOSIO_KEY_PROVIDER = "<PRIVATE KEY>"
+`)
 }
