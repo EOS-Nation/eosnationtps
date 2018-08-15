05ed49ee94e4beaadf08b145ef037b71a1df1488
Denis Carriere <carriere.denis@gmail.com>
2018-08-15 11:56:32 -0400

    move file

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # https://github.com/EOSIO/eosio.wasmsdk
-eosio-cpp -o eosnationtps.wasm src/eosnationtps.cpp
+eosio-cpp -o eosnationtps.wasm eosnationtps.cpp
 
 # Will be replaced soon with "eosio-cpp"
-eosiocpp -g abi/eosnationtps.abi src/eosnationtps.cpp
+eosiocpp -g abi/eosnationtps.abi eosnationtps.cpp
